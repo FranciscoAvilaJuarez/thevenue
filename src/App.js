7047e21b56da703876f573219b5490eb6dc31314
FranciscoAvilaJuarez <francisco.salomon.avila.juarez@gmail.com>
2019-06-12 13:56:09 -0500

    created needed folders

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './resources/styles.css'
+
+function App() {
+  return (
+    <div className="App">
+     hi, i missed you 
+    </div>
+  );
+}
+
+export default App;
